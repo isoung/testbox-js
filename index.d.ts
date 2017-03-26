4569ed218ba7e4a6869b96b4577a1940cb7b610f
isoung <isaiah131@gmail.com>
2017-03-26 17:55:09 -0500

    Fixed some tslint errors

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,11 @@
-export = testbox;
+export interface IScaffoldOptions {
+  projectName: string;
+  override: boolean;
+  programmingLanguage: string;
+  framework: string;
+  additionalOptions: string[];
+}
 
-declare namespace testbox {
-  export interface IScaffoldOptions {
-    projectName: string;
-    override: boolean;
-    programmingLanguage: string;
-    framework: string;
-    additionalOptions: string[];
-  }
-
-  export class Scaffolder {
-    init(options: IScaffoldOptions): Promise<boolean>
-  }
+export class Scaffolder {
+  public init(options: IScaffoldOptions): Promise<boolean>
 }
